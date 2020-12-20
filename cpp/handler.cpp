3bc886da49dc192b65558c1ec9ff7d4c64ebf423
mmacieje <mmaciejewski06@gmail.com>
2020-12-20 21:28:42 +0100

    Metric calculation without SHA256

--- cpp/handler.cpp
+++ cpp/handler.cpp
@@ -17,15 +17,25 @@ Handler::~Handler()
 
 void Handler::handlePost(http_request message)
 {
-    json::value input_json = message.extract_json().get();
     json::value response;
+    json::value input_json;
+    try
+    {
+        input_json = message.extract_json().get();
+    }
+    catch (const std::exception & ex)
+    {
+        message.reply(status_codes::BadRequest, response);
+        return ;
+    }
+
     try
     {
         response = calculateMetric(input_json);
     }
-    catch (...)
+    catch (const std::exception & ex)
     {
-        message.reply(status_codes::InternalError, response);
+        message.reply(status_codes::InternalError, ex.what());
         //message.reply(status_codes::BadRequest, response);
         return ;
     }
@@ -36,6 +46,28 @@ void Handler::handlePost(http_request message)
 json::value Handler::calculateMetric(json::value input)
 {
     json::value response = json::value::object();
-    response["foo"] = json::value::string("bar");
+
+    if(input.has_field("data") == false || input["data"].is_array() == false)
+    {
+        throw std::exception();
+    }
+    json::value data = input["data"];
+
+    utility::string_t concatenated_data;
+
+    int number_of_items = data.size();
+    for(int i=0; i<number_of_items;++i)
+    {
+        json::value el = data[i];
+        if(el.has_field("signature") == false || el["signature"].is_string() == false)
+        {
+            throw std::exception();
+        }
+        concatenated_data += el["signature"].as_string();
+    }
+    json::value metric = json::value::object();
+    metric["signature"] = json::value::string(concatenated_data);
+    metric["number-of-items"] = json::value::number(number_of_items);
+    response["metric"] = metric;
     return response;
 }
