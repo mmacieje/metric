9c47c6da9da7ea097dfb62b70f1e2b1741df50a5
mmacieje <mmaciejewski06@gmail.com>
2020-12-20 20:04:48 +0100

    Minor fix for linking. Changed server to listen on all interfaces. Added prompt for exiting

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -15,14 +15,16 @@ int main(int argc, char *argv[])
     std::unique_ptr<Handler> http_handler;
 
     utility::string_t port = U("9000");
-    utility::string_t address = U("http://127.0.0.1:");
+    utility::string_t address = U("http://0.0.0.0:");
     address.append(port);
     uri_builder uri(address);
     auto addr = uri.to_uri().to_string();
     http_handler = std::unique_ptr<Handler>(new Handler(addr));
     http_handler->open().wait();
 
-    while(true) { std::this_thread::sleep_for(std::chrono::seconds(5)); }
+    std::cout << "Server running. Press any key to exit." << std::endl;
+    std::string line;
+    std::getline(std::cin, line);
 
     http_handler->close().wait();
     return 0;
